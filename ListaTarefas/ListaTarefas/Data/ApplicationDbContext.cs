c26bf2b766a6dee1a63671e78fc38cad58b18147
itasouza <itasouza@yahoo.com.br>
2019-06-14 01:25:13 -0300

    Projeto Lista de Tarefas

---
+++ ListaTarefas/ListaTarefas/Data/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using ListaTarefas.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ListaTarefas.Data
+{
+    public class ApplicationDbContext:DbContext
+    {
+        public DbSet<Tarefas> TAREFA { get; set; }
+
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options): base(options)
+        {
+        }
+
+    }
+}
