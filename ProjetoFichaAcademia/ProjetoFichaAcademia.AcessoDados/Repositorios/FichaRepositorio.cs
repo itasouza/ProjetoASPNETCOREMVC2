1e0a7349f1d6175c48e5f723b07f1feb92d5a31c
itasouza <itasouza@yahoo.com.br>
2020-01-17 01:51:49 -0200

    arquivos do projeto

--- ProjetoFichaAcademia/ProjetoFichaAcademia.AcessoDados/Repositorios/FichaRepositorio.cs
+++ ProjetoFichaAcademia/ProjetoFichaAcademia.AcessoDados/Repositorios/FichaRepositorio.cs
@@ -3,6 +3,7 @@ using ProjetoFichaAcademia.AcessoDados.Interfaces;
 using ProjetoFichaAcademia.Dominio.Models;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
