76c47da7423a10ef3fa223254b17412ea20dc736
itasouza <itasouza@yahoo.com.br>
2020-01-30 00:29:35 -0200

    Projeto Aluguel de carros - adicionado

---
+++ AluguelCarro/AluguelCarro/Models/Context.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace AluguelCarro.Models
+{
+    public class Context : IdentityDbContext
+    {
+
+
+        public Context(DbContextOptions<Context> options)
+            : base(options)
+        {
+        }
+    }
+}
